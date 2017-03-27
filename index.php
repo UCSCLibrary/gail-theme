8ac43b6b9bd021e2d16db7d89f33a90e4c40621f
Nick Taber <nick.taber@gmail.com>
2017-03-27 10:47:11 -0700

    add simple pages and update index
    some missed pages from initial commit

--- index.php
+++ index.php
@@ -43,6 +43,23 @@
 	text-align: center;
 }
 
+/*CSS for the box colors*/
+.center-box{
+	width:220px;
+	height:200px;
+}
+
+.twt{
+	background:#f00;
+}
+.fb{
+	background:#3b5998;
+}
+.green{
+	background:#00f;
+}
+
+
 /* CSS for new carousel */
 .carousel-showsixmoveone {
   .carousel-control {
@@ -151,7 +168,7 @@
   <div class="row">
     <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
 
-    <!-- New carousel has minor issues: 
+    <!-- New carousel has minor issues:
         left and right move a bunch over but are only supposed to move one at a time.
         Based this carousel off of: http://codepen.io/rtpHarry/pen/ogVord-->
     <div class="container fluid" style="padding-top:50px;">
@@ -164,7 +181,7 @@
                 $items = get_recent_items(8);
                 set_loop_records('items',$items);
                 ?>
-                <?php 
+                <?php
                 foreach (loop('items') as $item):
                   if (array_search($item,$items) == 0){
                     echo "<div class='item active'>";
@@ -190,12 +207,13 @@
     </div>
 
     <div class="row">
+      <!--<div style="background: #D6DBDF">-->
       <h1>Project Highlights</h1>
       <div class="container fluid">
 
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
+            <img src="<?php echo img('Team.jpg', 'assets/img/frontpage');?>" class="img-responsive" style="max-height:215px;">
             <div class="caption" style="min-height:150px;">
               <h4>Introduction to the Gail Project</h4>
               <p>Learn more about the Gail Project and find out how you can help.</p>
@@ -204,7 +222,7 @@
         </div>
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
+            <img src="<?php echo img('StellaSign.jpg', 'assets/img/frontpage');?>" class="img-responsive" style="max-height:215px;">
             <div class="caption" style="min-height:150px;" >
               <h4>Do You have a Passport?</h4>
               <p>An essay and memoir about the project, written by one of our own alum, Stella Fronius</p>
@@ -213,7 +231,7 @@
         </div>
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
+            <img src="<?php echo img('Meeting.jpg', 'assets/img/frontpage');?>" class="img-responsive" style="max-height:215px;">
             <div class="caption" style="min-height:150px;">
               <h4>News & Events</h4>
               <p>Stay informed of our news and events relevant to the project.</p>
@@ -222,15 +240,16 @@
         </div>
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="thumbnail">
-            <img src="http://placehold.it/500x300" alt="">
+            <img src="<?php echo img('AlanRobert.jpg', 'assets/img/frontpage');?>" class="img-responsive" style="max-height:215px;">
             <div class="caption" style="min-height:150px;">
               <h4>Share Your History</h4>
-              <p>Your stories help us build context and develop meaningful narratives, 
+              <p>Your stories help us build context and develop meaningful narratives,
                 and are what allows us to draw conclusions and form expectations. Help us preserve your history.</p>
             </div>
           </div>
         </div>
       </div>
+    <!--</div>-->
     </div>
 
 
@@ -240,47 +259,90 @@
       <h1>Connect With Us</h1>
       <div class="container fluid">
         <div class="col-xs-6 col-sm-6 col-md-3">
-          Twitter
           <div class="row">
             <div class="thumbnail1 text-center">
-              <img src="http://placehold.it/220x200" alt="" class="center-block">
-              <div class="caption1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</p>
+			  <img src="<?php echo img('TW.jpg', 'assets/img');?>" alt="" class="center-block">
+                <a
+			       href="https://twitter.com/thegailproject">
+			       <img src="<?php echo img('twitter.png', 'assets/img');?>" alt="" class="img-responsive" style="position:absolute;bottom:160px;right:220px;margin:0;padding:5px 3px;height:15%;width:10%;">
+                </a>
+			  <div class="caption1">
+                <font color="white">
+				 <p>On our reading list! http://blog.lareviewofbooks.org
+				   /chinablog/journalism-love-wartime-
+				   china-qa-eve-hundred-midnights-author-
+				   bill-lascher/ ... h/t @mauracunningham
+				 </p>
+				</font>
               </div>
-            </div>
+			</div>
           </div>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-3">
-          Facebook
+       
+	   <div class="col-xs-6 col-sm-6 col-md-3">
           <div class="row">
             <div class="thumbnail1 text-center">
-              <img src="http://placehold.it/220x200" alt="" class="center-block">
-              <div class="caption1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</p>
+			  <img src="<?php echo img('FB.jpg', 'assets/img');?>" alt="" class="center-block">
+               <a
+			       href="https://www.facebook.com/TheGailProject/">
+			       <img src="<?php echo img('facebook.png', 'assets/img');?>" alt="" class="img-responsive" style="position:absolute;bottom:160px;right:220px;margin:0;padding:5px 3px;height:15%;width:10%;">
+			   </a>
+			  <div class="caption1">
+               <font color="white">
+				<p>
+				   More achievements 
+				   from the Gail Project students, 
+				   this time highlighting Stella Fronius! 
+				   http://news.ucsc.edu/2016/...
+				   /humanities-national-contest.html
+				</p>
+			   </font>
               </div>
-            </div>
+			</div>
           </div>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-3">
-          Instagram<!-- http://instafeedjs.com/ is a good resource for most recent. -->
+        
+		<div class="col-xs-6 col-sm-6 col-md-3">
+          <!-- http://instafeedjs.com/ is a good resource for most recent. -->
           <div class="row">
             <div class="thumbnail1 text-center">
-              <img src="http://placehold.it/220x200" alt="" class="center-block">
-              <div class="caption1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</p>
+			  <img src="<?php echo img('Inst.jpg', 'assets/img');?>" alt="" class="center-block">
+			    <a
+			       href="https://www.instagram.com/thegailproject/">
+			       <img src="<?php echo img('insta.png', 'assets/img');?>" alt="" class="img-responsive" style="position:absolute;bottom:160px;right:220px;margin:0;padding:5px 3px;height:15%;width:10%;">
+                </a>
+			  <div class="caption1">
+                <font color="white">
+				 <p>
+				    The Gail Project An Okinawan-American Dialogue Follow us for updates on our historical research, podcasts, YouTube channel, interviews, travel, team photos, and more!
+				 </p>
+			   </font>
               </div>
-            </div>
+             </a>
+			</div>
           </div>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-3">
-          Medium
-          <div class="row">
+        
+		<div class="col-xs-6 col-sm-6 col-md-3">
+          <!-- http://instafeedjs.com/ is a good resource for most recent. -->
+		  <div class="row">
             <div class="thumbnail1 text-center">
-              <img src="http://placehold.it/220x200" alt="" class="center-block">
-              <div class="caption1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, quisquam?</p>
+			     <img src="<?php echo img('Medium.jpg', 'assets/img');?>" alt="" class="center-block">
+              <a
+				 href="https://medium.com/@TheGailProject">
+				 <img src="<?php echo img('medium.png', 'assets/img');?>" alt="" class="img-responsive" style="position:absolute;bottom:160px;right:220px;margin:0;padding:5px 3px;height:15%;width:10%;">
+			  </a>
+			  <div class="caption1">
+               <font color="white">
+				<p>"By Command of Major General 
+					Eagles”: Archival Documents and Lived
+					Historical Experience
+				
+					   - By Alan Christy
+				</p>
+			   </font>
               </div>
-            </div>
+			</div>
           </div>
         </div>
       </div>
@@ -296,13 +358,15 @@
 <!-- Why is there an extra div? Who knows, but it fixes the footer width... -->
 </div>
 
-<div class="row" style="background-color:#3671d1;height:150px;margin-top:50px;">
-  <div class="container fluid" style="text-align: center;margin-top:65px;">
-    <a class="btn" href="/contribute.php">
-      Contribute
-    </a>
+<div class="row" style="background-color:#3671d1;height:200px;margin-top:40px;">
+
+  <div class="container fluid" style="text-align: center;margin-top:40px;">
+    <h1>Find out how you can help</h1>
+    <a class="btn btn-warning btn-lg" href="/contribute.php">Contribute</a>
   </div>
 </div>
+
+<?php echo foot(); ?>
 
 <script>//JS for recentItemCarousel
 (function(){
@@ -325,5 +389,3 @@
   });
 }());
 </script>
-
-<?php echo foot(); ?>
---
+++ simple-pages/about.php
@@ -0,0 +1,45 @@
+<style>
+#primary {
+    visibility: hidden;
+}
+</style>
+</div>
+<div class="container-fluid main-area">
+
+
+<div class="container-fluid" style="width:100%;padding-left:0px;padding-right:0px;">
+  <img src="<?php echo img('about.jpg', 'assets/img');?>" class="img-responsive">
+</div>
+
+
+<h3> Welcome to the Gail Project! </h3>
+
+<p> The Gail Project is a collaborative, international public history project that explores the founding years of the American military occupation of Okinawa.  The project is inspired by a collection of photos taken in Okinawa in 1952 by an American Army Captain: Charles Eugene Gail.  The photos were generously donated to Special Collections at McHenry Library by Charles' daughter, Geri Gail, and have since been made available for student research. </p>
+
+<p> Our team of faculty, artists and undergraduate students at the University of California, Santa Cruz, are developing a traveling exhibition of Gail's photographs with an accompanying digital archive that is comprised of the photos, key texts and documents, oral histories from both American and Okinawan voices, as well as undergraduate student research and writing.  We believe that using the photographs as a lens through which to view this crucial time is relevant to populations throughout Okinawa, Japan, the United States and the entire Pacific region, and we aim to establish a dialogue by shedding light on both historical and contemporary issues. </p>
+
+<p> The project emphasizes hands-on research and creation of content by undergraduate students and serves as an innovative platform for new educational methods that encourage the use of multimedia, social media, archival research and travel. </p>
+
+<p> The Gail Project is directed by Professor Alan Christy of UC Santa Cruz, and  Shelby Graham of the Sesnon Gallery. </p></B>
+
+
+
+<B><p>プロジェクトについて</p>
+
+
+
+<p>ゲイル•プロジェクトにようこそ！</p>
+
+<p>ゲイル•プロジェクトは、沖縄の米軍占領の創立時期の調査に関する国際公共歴史協同研究プロジェクトです。このプロジェクトのきっかけは、アメリカ陸軍大尉、チャールズ•ユージン•ゲイルが1952年に沖縄で撮影した写真のコレクションでした。それらの写真は、ゲイル大尉の娘、ジェリー•ゲイルさんの寛大な寄贈により、マクヘンリー図書館のスペシャルコレクションに収められ、学生の研究に利用されるようになりました。</p>
+
+<p>カリフォルニア大学サンタクルーズ校の教職員と学部生を含むチームは、ゲイル氏の写真集の移動展示会やデジタルアーカイブを開発しています。このアーカイブには、当時の写真や資料、アメリカと沖縄からのオーラル•ヒストリー、そして学部生の最近の研究論文等が保管されています。</p>
+
+<p>私たちは、写真を通してこの時代を見ることは、沖縄、日本、米国及び太平洋地域全体の人々のために重要であると考えています。また、そうすることによって歴史的、及び現代の問題点を解明するための対話の確立を目指しています。</p>
+<p>ゲイル•プロジェクトは学部生による実践的研究やコンテンツの作成に力を入れています。また、マルチメディア、ソーシャルメディア、アーカイブ研究、それと実習旅行などを奨励する、新たな革新的教育の場を提供しています。</p>
+
+
+
+<p>ゲイル•プロジェクトはカリフォルニア大学サンタクルーズ校準教授、アラン•クリスティー、および、セズノン•ギャラリー長のシェルビー•グラムによって管理されています。</p></B>
+
+
+</div>
