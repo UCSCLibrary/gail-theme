6370643c7e85c0780310898483b5f15e49112a16
Nicholas Austin Taber <nick.taber@gmail.com>
2017-04-19 14:57:39 -0700

    Add Content and CSS to Studio
    Added some placeholder records with sample copy.

--- simple-pages/studio.php
+++ simple-pages/studio.php
@@ -2,9 +2,90 @@
 #primary {
     display:none;
 }
+.rowmod{
+    margin-bottom:15px;
+}
 </style>
 </div>
 <div class="container-fluid main-area">
+    <div class="row">
+        <center>
+            <h1 class="col-md-12">
+                Project News & Events
+            </h1>
+        </center>
+    </div>
 
-The Studio is under construction...
+    <div class="row rowmod">
+        <div class="col-md-3">
+            <a href="https://gailproject.ucsc.edu/files/original/44f0205ef4e330779aa25db3a7e60f88.pdf">
+                <img src="https://gailproject.ucsc.edu/files/original/b17d48b97b5f7752bef25378fd3f9554.png" class="img-responsive"/>
+            </a>
+        </div>
+        <div class="col-md-9">
+            <h2>
+                title
+            </h2>
+            <h3>
+                subtitle...
+            </h3>
+            <p>
+                Lorem Ipsum dolor sit amet...
+            </p>
+        </div>
+    </div>
+    <hr/>
+
+    <div class="row rowmod">
+        <div class="col-md-3">
+            <a href="https://gailproject.ucsc.edu/files/original/44f0205ef4e330779aa25db3a7e60f88.pdf">
+                <img src="https://gailproject.ucsc.edu/files/original/b17d48b97b5f7752bef25378fd3f9554.png" class="img-responsive"/>
+            </a>
+        </div>
+        <div class="col-md-9">
+            <h2>
+                title
+            </h2>
+            <h3>
+                subtitle...
+            </h3>
+            <p>
+                Lorem Ipsum dolor sit amet...
+            </p>
+        </div>
+    </div>
+    <hr/>
+
+    <div class="row rowmod">
+        <div class="col-md-3">
+            <a href="https://gailproject.ucsc.edu/files/original/44f0205ef4e330779aa25db3a7e60f88.pdf">
+                <img src="https://gailproject.ucsc.edu/files/original/b17d48b97b5f7752bef25378fd3f9554.png" class="img-responsive"/>
+            </a>
+        </div>
+        <div class="col-md-9">
+            <h2>
+                title
+            </h2>
+            <h3>
+                subtitle...
+            </h3>
+            <p>
+                Lorem Ipsum dolor sit amet...
+            </p>
+        </div>
+    </div>
+
+
+    <!-- figure out if this'll work<div class="row">
+        <div class="col-md-3 pull-right">
+            <ul class="pagination">   
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+        </div>
+    </div>-->
+    
+</ul>
 </div>
