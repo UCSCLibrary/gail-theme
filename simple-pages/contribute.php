92c32efe01dd6a468bc2022043624435dcdc4131
Nicholas Austin Taber <nick.taber@gmail.com>
2017-04-19 14:25:49 -0700

    Add content to contribute
    Contribute now has stuff in it!

--- simple-pages/contribute.php
+++ simple-pages/contribute.php
@@ -1,1 +1,47 @@
-This is currently under construction
+<style>
+#primary {
+    display:none;
+}
+</style>
+</div>
+<div class="container-fluid main-area">
+    <div class="row-fluid">
+        <div class="container fluid">
+            <center>
+                <h1> Contribute </h1>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/WejH6naaNBc?ecver=1" frameborder="0" allowfullscreen></iframe>
+            </center>
+
+            <h2>
+                Giving Day is over, but you can still help out!
+            </h2>
+            <p>
+                You can help support the Gail Project in many ways, by contributing images, stories or financial support. 
+                Learn more about the supporting the Gail Project.
+            </p>
+            <p>
+                Please contact us if you are interested in the project and would like to share your story from Okinawa.
+            </p>
+
+            <p>
+                email:
+                <a href="mailto:thegailproject@gmail.com" target="_top">thegailproject@gmail.com</a>
+            </p>
+        </div>
+    </div>
+
+    <hr/>
+
+    <div class="row-fluid">
+        <div class="container fluid">
+            <p>
+            連絡先 沖縄のお話を聞かせて下さる方、ゲイル•プロジェクトに興味がおありの方はぜひご連絡下さい。 
+            </p>
+            <p>
+                電子メール:
+                <a href="mailto:thegailproject@gmail.com" target="_top">thegailproject@gmail.com</a>
+            </p>
+        </div>
+    </div>
+
+</div>
