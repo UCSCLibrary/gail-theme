7304264228d119c4d04bf424499cef286e8e8593
Nicholas Austin Taber <nick.taber@gmail.com>
2017-04-19 14:23:29 -0700

    Add CSS to Album
    Album page needed a whole bunch new CSS, including getting rid of the
    slug at the top and getting the images to display appropriately.

---
+++ simple-pages/album.php
@@ -0,0 +1,75 @@
+<style>
+#primary {
+  display:none;
+}
+.gail-photo{
+  background-color:#bfbfbf;
+  background: -webkit-linear-gradient(#ffffff, #bfbfbf); /* For Safari 5.1 to 6.0 */
+  background: -o-linear-gradient(#ffffff, #bfbfbf); /* For Opera 11.1 to 12.0 */
+  background: -moz-linear-gradient(#ffffff, #bfbfbf); /* For Firefox 3.6 to 15 */
+  background: linear-gradient(#ffffff, #bfbfbf); /* Standard syntax */
+  border-radius:5px;
+}
+.photo-category{
+  font-size: 150%;
+}
+</style>
+</div>
+<div class="container-fluid main-area">
+  <center>
+    <div class="row">
+      <h1>
+        The Gail Photos
+      </h1>
+
+      
+      <div id="gail-photos" class="container-fluid">
+        <div class="gail-photo col-xs-12 col-md-2 col-md-offset-1">
+          <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid">
+            <img src="/files/original/6d8bd5229e20fc6ffd99d7b6647cd9b4.png" class="img-responsive"/>
+          </a>
+          <hr/>
+          <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid" class="photo-category">
+            Daily Life & Work
+          </a>
+        </div>
+        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
+          <a href="/items/browse?tags=Heritage&view=grid">
+            <img src="/files/original/8837003f366b6559a16e68efe05d274a.png" class="img-responsive"/>
+          </a>
+          <hr/>
+          <a href="/items/browse?tags=Heritage&view=grid"class="photo-category">
+            Heritage
+          </a>
+        </div>
+        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
+          <a href="/items/browse?tags=Ocean+Life&view=grid">
+            <img src="/files/original/6a3dc4bd056a2c066b38fb13fb45478c.png" class="img-responsive"/>
+          </a>
+          <hr/>
+          <a href="/items/browse?tags=Ocean+Life&view=grid"class="photo-category">
+            Ocean Life
+          </a>
+        </div>
+        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
+          <a href="/items/browse?tags=People&view=grid">
+            <img src="/files/original/9c5cec1875e514150223e97f9af51a95.png" class="img-responsive"/>
+          </a>
+          <hr/>
+          <a href="/items/browse?tags=People&view=grid"class="photo-category">
+            People
+          </a>
+        </div>
+        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
+          <a href="/items/browse?tags=Landscapes&view=grid">
+            <img src="/files/original/65a6d73fcbdb0d5122e2b27581c2c888.png" class="img-responsive"/>
+          </a>
+          <hr/>
+          <a href="/items/browse?tags=Landscapes&view=grid"class="photo-category">
+            Landscapes
+          </a>
+        </div>
+      </div>
+    </div>
+  </center>
+</div>
