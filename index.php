acb871f9bd566acb867cbb1807c924a89ea20190
Nicholas Austin Taber <nick.taber@gmail.com>
2017-04-19 13:10:09 -0700

    Modify Contribute Link on Index
    link pointed to /contribute.php, needed to point to /contribute.

--- index.php
+++ index.php
@@ -361,7 +361,7 @@
 
   <div class="container fluid" style="text-align: center;margin-top:40px;">
     <h1>Find out how you can help</h1>
-    <a class="btn btn-warning btn-lg" href="/contribute.php">Contribute</a>
+    <a class="btn btn-warning btn-lg" href="/contribute">Contribute</a>
   </div>
 </div>
 
