066851b11bf24203915c3dc2fc36a1711df2f037
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-23 15:58:42 -0700

    Swap to CDN as main CSS
    Best practices dictate using CDN as primary and local as a backup.

--- common/header.php
+++ common/header.php
@@ -19,8 +19,9 @@
 
     <!-- Le styles -->
 
+    <!---->
     <?php queue_css_file('bootstrap.min','all',false,'bootstrap-dist/3.2.0/css'); ?>
-    <?php queue_css_file(array('docs','style')); ?>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
 
@@ -52,9 +53,8 @@
 
 <style>
   body {
-        xpadding-top: 60px;
-        xpadding-bottom: 40px;
         padding-top:0px;
+        background-image:none;
       }
       .sidebar-nav {
         xpadding: 9px 0;
