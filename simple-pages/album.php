3757cd3bc5d3daaa8587913ab98c71c180fa21a0
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-23 15:54:09 -0700

    Update visuals of Album page
    Needed to be spiffier.

--- simple-pages/album.php
+++ simple-pages/album.php
@@ -2,74 +2,106 @@
 #primary {
   display:none;
 }
-.gail-photo{
-  background-color:#bfbfbf;
-  background: -webkit-linear-gradient(#ffffff, #bfbfbf); /* For Safari 5.1 to 6.0 */
-  background: -o-linear-gradient(#ffffff, #bfbfbf); /* For Opera 11.1 to 12.0 */
-  background: -moz-linear-gradient(#ffffff, #bfbfbf); /* For Firefox 3.6 to 15 */
-  background: linear-gradient(#ffffff, #bfbfbf); /* Standard syntax */
-  border-radius:5px;
+
+.navbar {
+  margin-bottom: 0;
 }
-.photo-category{
-  font-size: 150%;
+
+.jumbotron {
+  padding-top: 6rem;
+  padding-bottom: 6rem;
+  margin-bottom: 0;
+  background-color: #fff;
+}
+
+.jumbotron p:last-child {
+  margin-bottom: 0;
+}
+.album {
+  min-height: 50rem; /* Can be removed; just added for demo purposes */
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background-color: #f7f7f7;
+}
+
+.jumbotron-heading {
+  font-weight: 300;
+}
+
+.card {
+  float: left;
+  width: 33.333%;
+  padding: .75rem;
+  margin-bottom: 2rem;
+  border: 0;
+  background-color: #fff;
+}
+
+.card > img {
+  margin-bottom: .75rem;
+}
+
+.card-text {
+  font-size: 85%;
+  text-align: center;
 }
 </style>
 </div>
-<div class="container-fluid main-area">
-  <center>
-    <div class="row">
-      <h1>
-        The Gail Photos
-      </h1>
+<div class="container-fluid main-area" style="padding-bottom:50px;">
 
-      
-      <div id="gail-photos" class="container-fluid">
-        <div class="gail-photo col-xs-12 col-md-2 col-md-offset-1">
-          <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid">
-            <img src="/files/original/6d8bd5229e20fc6ffd99d7b6647cd9b4.png" class="img-responsive"/>
-          </a>
-          <hr/>
-          <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid" class="photo-category">
-            Daily Life & Work
-          </a>
+    <div class="row">
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">The Gail Photos</h1>
+          <p class="lead text-muted">Blah blah blah Mr. Gail Blah blah copy</p>
         </div>
-        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
-          <a href="/items/browse?tags=Heritage&view=grid">
-            <img src="/files/original/8837003f366b6559a16e68efe05d274a.png" class="img-responsive"/>
-          </a>
-          <hr/>
-          <a href="/items/browse?tags=Heritage&view=grid"class="photo-category">
-            Heritage
-          </a>
-        </div>
-        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
-          <a href="/items/browse?tags=Ocean+Life&view=grid">
-            <img src="/files/original/6a3dc4bd056a2c066b38fb13fb45478c.png" class="img-responsive"/>
-          </a>
-          <hr/>
-          <a href="/items/browse?tags=Ocean+Life&view=grid"class="photo-category">
-            Ocean Life
-          </a>
-        </div>
-        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
-          <a href="/items/browse?tags=People&view=grid">
-            <img src="/files/original/9c5cec1875e514150223e97f9af51a95.png" class="img-responsive"/>
-          </a>
-          <hr/>
-          <a href="/items/browse?tags=People&view=grid"class="photo-category">
-            People
-          </a>
-        </div>
-        <div class="gail-photo col-xs-12 col-md-2" style="margin-left:5px;">
-          <a href="/items/browse?tags=Landscapes&view=grid">
-            <img src="/files/original/65a6d73fcbdb0d5122e2b27581c2c888.png" class="img-responsive"/>
-          </a>
-          <hr/>
-          <a href="/items/browse?tags=Landscapes&view=grid"class="photo-category">
-            Landscapes
-          </a>
+      </section>
+
+      <div class="album text-muted">
+        <div class="container-fluid">
+          <div class="row-fluid">
+            <!-- One Container of 3, one of 2-->
+            <div class="container-fluid">
+              <div class="card">
+                <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid">
+                  <img src="/files/original/6d8bd5229e20fc6ffd99d7b6647cd9b4.png" class="img-responsive"/>
+                </a>
+                <p class="card-text">Daily Life and Work</p>
+              </div>
+
+
+              <div class="card">
+                <a href="/items/browse?tags=Heritage&view=grid">
+                  <img src="/files/original/8837003f366b6559a16e68efe05d274a.png" class="img-responsive"/>
+                </a>
+                <p class="card-text">Heritage</p>
+              </div>
+
+              <div class="card">
+                <a href="/items/browse?tags=Ocean+Life&view=grid">
+                  <img src="/files/original/6a3dc4bd056a2c066b38fb13fb45478c.png" class="img-responsive"/>
+                </a>
+                <p class="card-text">Maritime Life</p>
+              </div>
+            </div>
+            <div class="container-fluid">
+              <div class="card">
+                <a href="/items/browse?tags=People&view=grid">
+                  <img src="/files/original/9c5cec1875e514150223e97f9af51a95.png" class="img-responsive"/>
+                </a>
+                <p class="card-text">People</p>
+              </div>
+
+              <div class="card">
+                <a href="/items/browse?tags=Landscapes&view=grid">
+                  <img src="/files/original/65a6d73fcbdb0d5122e2b27581c2c888.png" class="img-responsive"/>
+                </a>
+                <p class="card-text">Landscapes</p>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
-  </center>
 </div>
