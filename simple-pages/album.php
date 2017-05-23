fdccd87d82e306ed3490d3499abe00573c178af2
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-23 16:09:32 -0700

    Further spiffification

--- simple-pages/album.php
+++ simple-pages/album.php
@@ -30,7 +30,6 @@
 
 .card {
   float: left;
-  width: 33.333%;
   padding: .75rem;
   margin-bottom: 2rem;
   border: 0;
@@ -42,7 +41,7 @@
 }
 
 .card-text {
-  font-size: 85%;
+  font-size:120%;
   text-align: center;
 }
 </style>
@@ -62,36 +61,43 @@
           <div class="row-fluid">
             <!-- One Container of 3, one of 2-->
             <div class="container-fluid">
+              <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid">
                   <img src="/files/original/6d8bd5229e20fc6ffd99d7b6647cd9b4.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Daily Life and Work</p>
               </div>
+              </div>
 
-
+              <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Heritage&view=grid">
                   <img src="/files/original/8837003f366b6559a16e68efe05d274a.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Heritage</p>
               </div>
+              </div>
 
+              <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Ocean+Life&view=grid">
                   <img src="/files/original/6a3dc4bd056a2c066b38fb13fb45478c.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Maritime Life</p>
               </div>
-            </div>
-            <div class="container-fluid">
+              </div>
+
+              <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=People&view=grid">
                   <img src="/files/original/9c5cec1875e514150223e97f9af51a95.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">People</p>
               </div>
+              </div>
 
+              <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Landscapes&view=grid">
                   <img src="/files/original/65a6d73fcbdb0d5122e2b27581c2c888.png" class="img-responsive"/>
@@ -99,6 +105,7 @@
                 <p class="card-text">Landscapes</p>
               </div>
             </div>
+          </div>
 
           </div>
         </div>
