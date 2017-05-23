030ba65f7b1d411a75b6674a2ac19916a7b2f4a5
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-23 16:17:23 -0700

    Fix readability of code

--- simple-pages/album.php
+++ simple-pages/album.php
@@ -2,32 +2,27 @@
 #primary {
   display:none;
 }
-
 .navbar {
   margin-bottom: 0;
 }
-
 .jumbotron {
   padding-top: 6rem;
   padding-bottom: 6rem;
   margin-bottom: 0;
   background-color: #fff;
 }
-
 .jumbotron p:last-child {
   margin-bottom: 0;
 }
 .album {
-  min-height: 50rem; /* Can be removed; just added for demo purposes */
+  min-height: 50rem;
   padding-top: 3rem;
   padding-bottom: 3rem;
   background-color: #f7f7f7;
 }
-
 .jumbotron-heading {
   font-weight: 300;
 }
-
 .card {
   float: left;
   padding: .75rem;
@@ -35,11 +30,9 @@
   border: 0;
   background-color: #fff;
 }
-
 .card > img {
   margin-bottom: .75rem;
 }
-
 .card-text {
   font-size:120%;
   text-align: center;
@@ -47,57 +40,56 @@
 </style>
 </div>
 <div class="container-fluid main-area" style="padding-bottom:50px;">
+  <div class="row">
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">The Gail Photos</h1>
+        <p class="lead text-muted">Blah blah blah Mr. Gail Blah blah copy</p>
+      </div>
+    </section>
 
-    <div class="row">
-      <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">The Gail Photos</h1>
-          <p class="lead text-muted">Blah blah blah Mr. Gail Blah blah copy</p>
-        </div>
-      </section>
-
-      <div class="album text-muted">
-        <div class="container-fluid">
-          <div class="row-fluid">
-            <!-- One Container of 3, one of 2-->
-            <div class="container-fluid">
-              <div class="col-lg-3 col-md-6 col-sm-12">
+    <div class="album text-muted">
+      <div class="container-fluid">
+        <div class="row-fluid">
+          <!-- One Container of 3, one of 2-->
+          <div class="container-fluid">
+            <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Daily+Life+%26+Work&view=grid">
                   <img src="/files/original/6d8bd5229e20fc6ffd99d7b6647cd9b4.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Daily Life and Work</p>
               </div>
-              </div>
+            </div>
 
-              <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Heritage&view=grid">
                   <img src="/files/original/8837003f366b6559a16e68efe05d274a.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Heritage</p>
               </div>
-              </div>
+            </div>
 
-              <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Ocean+Life&view=grid">
                   <img src="/files/original/6a3dc4bd056a2c066b38fb13fb45478c.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">Maritime Life</p>
               </div>
-              </div>
+            </div>
 
-              <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=People&view=grid">
                   <img src="/files/original/9c5cec1875e514150223e97f9af51a95.png" class="img-responsive"/>
                 </a>
                 <p class="card-text">People</p>
               </div>
-              </div>
+            </div>
 
-              <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="col-lg-3 col-md-6 col-sm-12">
               <div class="card">
                 <a href="/items/browse?tags=Landscapes&view=grid">
                   <img src="/files/original/65a6d73fcbdb0d5122e2b27581c2c888.png" class="img-responsive"/>
@@ -106,9 +98,8 @@
               </div>
             </div>
           </div>
-
-          </div>
         </div>
       </div>
     </div>
+  </div>
 </div>
