e6f51179e445d24464d2aeb61dfdbb92e444bc14
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-24 14:21:36 -0700

    Remove static CSS load
    Seemed to be causing issue with our carousel. Will remedy this later but
    for now only CDN will be used for bootstrap CSS.

--- common/header.php
+++ common/header.php
@@ -19,10 +19,9 @@
 
     <!-- Le styles -->
 
-    <!--
-    <?php queue_css_file('bootstrap.min','all',false,'bootstrap-dist/3.2.0/css'); ?>-->
     <!-- Load in carousel css, javascript in footer. -->
     <?php queue_css_file('manymoveonecarousel'); ?>
+    <!-- load in bootstrap via CDN -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
