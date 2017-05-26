e078c9edad6335671c61dacb5ccfac40bdd2d08b
Nicholas Austin Taber <nick.taber@gmail.com>
2017-05-25 19:28:35 -0700

    Playing around with Vault stylings
    Adding more modern style to the Vault page

--- simple-pages/vault.php
+++ simple-pages/vault.php
@@ -2,9 +2,46 @@
 #primary {
     display:none;
 }
+
+.navbar {
+  margin-bottom: 0;
+}
+.jumbotron {
+  margin-bottom: 0px;
+  height: 350px;
+  color: white;
+  text-shadow: black 0.3em 0.3em 0.3em;
+  background-size:cover;
+  background:url('/themes/Bootstrapv1/assets/img/vault3.jpg') no-repeat center center;
+}
 </style>
 </div>
-<div class="container-fluid main-area">
 
-The Vault is under construction...
+<div class="container-fluid main-area" style="padding-bottom:50px;">
+  <div class="row">
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">The Vault</h1>
+        <p class="lead text-muted">We opened the vaults of Okinawa, for your perusal</p>
+      </div>
+    </section>
+
+<div class="container-fluid">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh felis, tempor a felis accumsan, aliquet pulvinar erat. Aliquam erat volutpat. Cras laoreet massa sit amet risus tincidunt iaculis. Pellentesque hendrerit tortor eget congue porta. Proin scelerisque interdum faucibus. Nunc ut sollicitudin elit, at molestie libero. Mauris et congue nunc, sit amet dignissim enim. Donec efficitur tincidunt leo, non fringilla arcu egestas ac. Morbi a augue condimentum, iaculis diam accumsan, ultricies dui. Sed commodo lorem id tortor mattis venenatis. Aliquam sed accumsan nulla. Vivamus nec ante ex. Phasellus quis aliquet tortor. Proin eu convallis odio, malesuada hendrerit tortor.
+</p>
+<p>
+Cras a felis et lacus sodales faucibus. Suspendisse ac dolor malesuada, aliquet risus at, ornare leo. Nam fringilla tellus eu nulla placerat, nec lobortis mi mollis. In in dapibus est. Mauris pulvinar accumsan est, sit amet blandit dolor. Nunc eget erat et mauris molestie euismod. Integer leo nisi, maximus ullamcorper mauris vel, interdum commodo sem. Proin faucibus leo sed blandit hendrerit. Vivamus tempor risus ut mattis congue. Nunc ac elementum mi. Nam molestie ligula viverra mattis placerat. Nunc mattis varius turpis a malesuada.
+</p>
+<p>
+Nulla non turpis quam. Fusce auctor justo eu sapien hendrerit finibus. Curabitur quis nibh sit amet tortor imperdiet imperdiet a non elit. Fusce ornare luctus vulputate. In hac habitasse platea dictumst. In egestas arcu a augue bibendum, nec rhoncus odio lacinia. Morbi pretium molestie dolor in dictum. Nunc eu nunc eget mauris vestibulum lacinia. Curabitur commodo congue enim et posuere. Suspendisse nec ligula vel sapien bibendum vulputate. Maecenas ac tortor at erat dapibus posuere auctor at lorem.
+</p>
+<p>
+Nulla gravida auctor sem. Curabitur at eros massa. Maecenas mollis sem nec augue porta ultrices. Ut fringilla tristique quam et rhoncus. Sed malesuada, neque at molestie lobortis, ipsum nibh ornare nunc, eget fermentum sapien ex sit amet quam. Praesent gravida massa eu nibh suscipit, ac convallis tortor vehicula. Nulla ipsum nunc, posuere eu condimentum vel, mattis auctor justo. In pellentesque diam et orci iaculis, a commodo velit pellentesque. Nulla faucibus aliquet orci quis ullamcorper.
+</p>
+<p>
+Duis facilisis, ante id elementum rhoncus, justo quam pellentesque tortor, nec tristique lacus massa a dui. Donec ex tellus, porta a faucibus ac, eleifend ac est. In in feugiat dui. Sed sit amet volutpat mi, eget tincidunt dolor. Phasellus tristique tellus nec augue commodo aliquam. Vivamus ac nisl vitae sapien ultricies egestas. Sed vitae euismod augue. Etiam ac nibh aliquet, venenatis quam vitae, malesuada lectus. Mauris rhoncus nisi fringilla lectus tincidunt, nec placerat ante consectetur. Nam eu lobortis velit. Cras quam massa, feugiat eget vehicula et, porta id tellus. Nullam pellentesque ornare consectetur.
+</p>
+</div>
+</div>
 </div>
