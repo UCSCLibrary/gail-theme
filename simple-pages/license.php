11f14933e85f581a214aae9409957ea5adf06b31
Nicholas Austin Taber <nick.taber@gmail.com>
2017-08-11 13:10:21 -0700

    Add license

---
+++ simple-pages/license.php
@@ -0,0 +1,22 @@
+</div>
+
+<div class="container-fluid main-area" style="padding-bottom:10rem;">
+    <div class="row-fluid">
+        <div class="container fluid">
+            <div class="jumbotron text-center">
+              <div class="container">
+                <h1 class="jumbotron-heading">License Agreement</h1></p>
+              </div>
+            </div>
+            
+            <hr/>
+            <p>
+            	I am contributing this work and irrevocably grant a non-exclusive, perpetual, royalty-free, worldwide license for this work to the University of California Regents to display, distribute, reproduce, perform, or create derivatives works based upon it.
+            </p>
+
+			<p>
+				Contributions to the Gail Project will be stored on our servers. When granted by the contributor, the audio will be made publicly available to any user of the internet.  At a minimum, we require that contributors grant a license to the University of California Regents so that the UC Santa Cruz Libraries and the Gail Project volunteers can manage and preserve the items in accordance with their policies and best practices.
+			</p>
+        </div>
+    </div>
+</div>
